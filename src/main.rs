1f68e5faaba113ef3ae2119847c032593800044e
Zelly Snyder <zelly@outlook.com>
2021-12-24 21:21:21 -0800

    read streaming from stdin as well
    	This helps if a user has a script that generates new names on the
    	fly. Then, the user can simply pipe the output to this program,
    	and it will check the availability of each generated name as it goes.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,10 @@
 use clap::{AppSettings, Parser, Subcommand};
-use std::path::PathBuf;
+use std::{io::BufRead, path::PathBuf};
 use web3::{
     contract::{Contract, Options},
     types::Address,
     Transport,
 };
-
-mod file_reader {
-    use std::{
-        fs::File,
-        io::{self, prelude::*},
-    };
-
-    pub struct BufReader {
-        reader: io::BufReader<File>,
-    }
-
-    impl BufReader {
-        pub fn open(path: impl AsRef<std::path::Path>) -> io::Result<Self> {
-            let f = File::open(path)?;
-            let reader = io::BufReader::new(f);
-            Ok(Self { reader })
-        }
-
-        pub fn read_line<'buf>(
-            &mut self,
-            buffer: &'buf mut String,
-        ) -> Option<io::Result<&'buf mut String>> {
-            buffer.clear();
-            self.reader
-                .read_line(buffer)
-                .map(|u| if u == 0 { None } else { Some(buffer) })
-                .transpose()
-        }
-    }
-}
 
 #[derive(Parser)]
 #[clap(name = "ens-name-search")]
@@ -46,11 +16,11 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    // Check the availability of one name
+    /// Check the availability of one name
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     Single { name: String },
 
-    // Check the availability of many names, given by a line-deliminted file
+    /// Check the availability of many names, given by a line-delimited file, or "-" for stdin
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     Batch {
         #[clap(required = true, parse(from_os_str))]
@@ -82,10 +52,10 @@ async fn main() -> web3::contract::Result<()> {
         std::env::var("ETH_NODE_RPC").expect("Missing ETH_NODE_RPC environment variable");
     let transport = web3::transports::WebSocket::new(&transport_uri).await?;
     let web3 = web3::Web3::new(transport);
-    let CONTROLLER_ADDRESS: Address = "283Af0B28c62C092C9727F1Ee09c02CA627EB7F5".parse().unwrap();
+    let controller_address: Address = "283Af0B28c62C092C9727F1Ee09c02CA627EB7F5".parse().unwrap();
     let controller_contract = Contract::from_json(
         web3.eth(),
-        CONTROLLER_ADDRESS,
+        controller_address,
         include_bytes!("../resources/controller_abi.json"),
     )?;
 
@@ -100,11 +70,26 @@ async fn main() -> web3::contract::Result<()> {
             }
         }
         Commands::Batch { path } => {
-            let path = path.as_path();
-            if !path.exists() {
-                eprintln!("\"{}\" does not exist", path.display());
+            match &path.to_str().unwrap() {
+                // Accept from stdin
+                &"-" => {
+                    process_batch_stdin(&controller_contract).await.unwrap();
+                }
+
+                // Accept line-delimited file
+                _ => {
+                    let path = path.as_path();
+                    if !path.exists() {
+                        eprintln!("\"{}\" does not exist", path.display());
+                        std::process::exit(1);
+                    }
+                    let f = std::fs::File::open(path).unwrap_or_else(|e| {
+                        eprintln!("Could not open file at \"{}\": {:?}", path.display(), e);
+                        std::process::exit(1);
+                    });
+                    process_batch(&controller_contract, f).await.unwrap();
+                }
             }
-            process_batch(&controller_contract, path).await.unwrap();
         }
     }
     Ok(())
@@ -112,14 +97,26 @@ async fn main() -> web3::contract::Result<()> {
 
 async fn process_batch<T: Transport>(
     controller_contract: &Contract<T>,
-    path: impl AsRef<std::path::Path>,
+    file_handle: impl std::io::Read,
 ) -> std::io::Result<()> {
-    let mut reader = file_reader::BufReader::open(path)?;
-    let mut buffer = String::new();
+    let reader = std::io::BufReader::new(file_handle);
 
-    while let Some(line) = reader.read_line(&mut buffer) {
-        let line = line?;
-        let line = clean_name(line);
+    for line in reader.lines() {
+        let line = clean_name(&line?);
+        let avail = available(&controller_contract, &line).await.unwrap();
+        if avail {
+            println!("{}", line);
+        }
+    }
+    Ok(())
+}
+
+async fn process_batch_stdin<T: Transport>(
+    controller_contract: &Contract<T>,
+) -> std::io::Result<()> {
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let line = clean_name(&line?);
         let avail = available(&controller_contract, &line).await.unwrap();
         if avail {
             println!("{}", line);
